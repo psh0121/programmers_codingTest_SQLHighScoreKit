cf25e6d20340990ef91c85749e8a3afd1fb6cbbc
psh0121 <tngus2057@gmail.com>
2023-09-03 14:30:04 +0900

    [SELECT] 인기있는 아이스크림

---
+++ SELECT/select-6.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133024
+
+-- mysql
+select flavor
+from first_half
+order by total_order desc, shipment_id asc;
+
+-- oracle
+select flavor
+from first_half
+order by total_order desc, shipment_id asc;
