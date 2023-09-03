2c98269cc79861939b1b235d0ecf172e6fb953ba
psh0121 <tngus2057@gmail.com>
2023-09-03 15:01:48 +0900

    [SELECT] 모든 레코드 조회하기

---
+++ SELECT/select-11.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59034
+
+-- mysql
+select *
+from animal_ins
+order by animal_id;
+
+-- oracle
+select *
+from animal_ins
+order by animal_id;
