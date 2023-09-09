3a6d39b5ab2f38aeb4fa14e6528a5be33891707a
psh0121 <tngus2057@gmail.com>
2023-09-09 09:03:51 +0900

    [SELECT] 어린 동물 찾기

---
+++ SELECT/select-16.sql
@@ -0,0 +1,14 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59037
+
+-- mysql
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NOT INTAKE_CONDITION = 'Aged'
+ORDER BY ANIMAL_ID;
+
+
+-- oracle
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NOT INTAKE_CONDITION = 'Aged'
+ORDER BY ANIMAL_ID;
