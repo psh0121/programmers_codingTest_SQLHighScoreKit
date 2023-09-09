1935d5870c2f66661e2269d62f8852dd69a49cb7
psh0121 <tngus2057@gmail.com>
2023-09-09 10:01:55 +0900

    [IS NULL] 이름이 있는 동물의 아이디

---
+++ IS NULL/is null-3.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59407
+
+-- mysql
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID;
+
+-- oracle
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID;
