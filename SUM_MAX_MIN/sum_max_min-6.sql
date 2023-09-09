31a3396ac714ad745cd2337f17ae19df53ba8385
psh0121 <tngus2057@gmail.com>
2023-09-09 09:45:45 +0900

    [SUM_MAX_MIN] 중복 제거하기

---
+++ SUM_MAX_MIN/sum_max_min-6.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59408
+
+-- mysql
+SELECT COUNT(DISTINCT NAME)
+FROM ANIMAL_INS;
+
+-- oracle
+SELECT COUNT(DISTINCT NAME)
+FROM ANIMAL_INS;
