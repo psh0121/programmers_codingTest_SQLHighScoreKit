ea3fcddfd1f851e2fc4384533996968bb2114647
psh0121 <tngus2057@gmail.com>
2023-09-09 09:05:59 +0900

    [SELECT] 동물의 아이디와 이름

---
+++ SELECT/select-17.sql
@@ -0,0 +1,12 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59403
+
+-- mysql
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+
+-- oracle
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
