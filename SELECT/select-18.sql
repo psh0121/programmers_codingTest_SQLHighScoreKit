a117737dd0dcb6678f04f78c524ec71bc1ebb73d
psh0121 <tngus2057@gmail.com>
2023-09-09 09:09:25 +0900

    [SELECT] 여러 기준으로 정렬하기

---
+++ SELECT/select-18.sql
@@ -0,0 +1,12 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59404
+
+-- mysql
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
+
+
+-- oracle
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
