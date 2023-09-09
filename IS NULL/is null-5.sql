be2e4508f5cd523e3c0ce38ae81efd8baf09f8c0
psh0121 <tngus2057@gmail.com>
2023-09-09 10:09:14 +0900

    [IS NULL] 나이 정보가 없는 회원 수 구하기

---
+++ IS NULL/is null-5.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131528?language=mysql
+
+-- mysql
+SELECT COUNT(*)
+FROM USER_INFO
+WHERE AGE IS NULL;
+
+-- oracle
+SELECT COUNT(*)
+FROM USER_INFO
+WHERE AGE IS NULL;
