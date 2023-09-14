9ead29e66e26464951e714d159e4d4879cf152ef
psh0121 <tngus2057@gmail.com>
2023-09-14 18:31:04 +0900

    [STRING_DATE] 루시와 엘라 찾기

---
+++ STRING_DATE/string_date-3.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59046
+
+-- mysql
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
+
+-- oracle
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
