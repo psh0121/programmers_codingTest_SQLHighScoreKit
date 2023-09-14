05fc6df5f53bb75524ffd91828d60ef6386d042e
psh0121 <tngus2057@gmail.com>
2023-09-14 18:42:18 +0900

    [STRING_DATE] 이름에 el이 들어가는 동물 찾기

---
+++ STRING_DATE/string_date-4.sql
@@ -0,0 +1,13 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59047
+
+-- mysql
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE = 'DOG'
+ORDER BY NAME;
+
+-- oracle
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE LOWER(NAME) LIKE '%el%' AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME;
